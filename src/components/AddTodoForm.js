91eded34c75c53f946e60ffcbf3f147f95e3ec01
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-21 20:23:34 -0700

    Add mapDispatchToProps

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -14,6 +14,7 @@ class AddTodoForm extends Component {
 
   submitForm = (e) => {
     e.preventDefault()
+    this.props.addToDo(this.state.todo)
     this.setState({ todo: '' });
   }
 
@@ -31,5 +32,9 @@ class AddTodoForm extends Component {
     )
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  addToDo: text => dispatch( addToDo(text) )
+})
 
 export default AddTodoForm;
