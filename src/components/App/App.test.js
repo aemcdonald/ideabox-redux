82b28042a60860bfd07192f8f9aaf90c836dd0ea
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-24 13:00:52 -0700

    Create initial passing test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import App from './App';
-import { render screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+describe('App', () => {
+    it('Should pass an initial test', () => {
+        expect(true).toBe(true)
+    })
+})
